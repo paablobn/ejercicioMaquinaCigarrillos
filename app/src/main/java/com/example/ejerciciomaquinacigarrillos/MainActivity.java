0d29e7e41bf1d26734236a00ad951e168f2c0ad8
pablobn <pablo.bruixola.navarro@cieep.com>
2022-11-08 11:14:35 +0100

    Cigarillos terminado

--- app/src/main/java/com/example/ejerciciomaquinacigarrillos/MainActivity.java
+++ app/src/main/java/com/example/ejerciciomaquinacigarrillos/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejerciciomaquinacigarrillos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ejerciciomaquinacigarrillos.databinding.ActivityMainBinding;
 
@@ -11,11 +12,31 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private int numCigarrillos = 20;
+    private float dinero;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.txtNumCigarrilos.setText(String.valueOf(numCigarrillos));
+        binding.txtDineroCigarrilos.setText(String.valueOf(dinero));
+        binding.btnUsarMaquina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (numCigarrillos >1){
+                    numCigarrillos--;
+                    binding.txtNumCigarrilos.setText(String.valueOf(numCigarrillos));
+                }else {
+                    dinero = dinero +5;
+                    numCigarrillos = 20;
+                    binding.txtNumCigarrilos.setText(String.valueOf(numCigarrillos));
+                    binding.txtDineroCigarrilos.setText(String.valueOf(dinero));
+                }
+            }
+        });
     }
 }
